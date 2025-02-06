7f1e3249f68adbccbb9727c2c882773e7d118a0a
Lucas <lucassanabriac@gmail.com>
2025-02-06 15:24:52 -0300

    Add a slider to dynamically adjust the grid size

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const container = document.getElementById("container");
+  let tam = 16;
 
-for (let j = 0; j < 16; j++) {
-    let newRow = document.createElement("div"); // Crea un div
-    newRow.style.display = "flex";
-    for (let i = 0; i < 16; i++) {
-        let newDiv = document.createElement("div"); // Crea un div
-        newDiv.textContent = ""; // Agrega texto al div
-        newDiv.className = "caja"; // Se aplica el estilo definido en CSS
-        newDiv.style.border = "1px solid black";
-        newDiv.style.margin = "0px";
-        newDiv.style.padding = "5px";
-        newDiv.style.width = "10px";
-        newDiv.style.height = "10px";
-        newRow.appendChild(newDiv); // Agrega el div al body
-    }
-    container.appendChild(newRow); // Agrega el div al body
-}
+  function actualizarValor(valor) {
+      document.getElementById("valor").textContent = valor;
+      tam = valor;
+      changeSize(tam);
+  }
+
+  function changeSize(tam) {
+      container.innerHTML = ""; // Limpia el contenedor
+
+      for (let j = 0; j < tam; j++) {
+          let newRow = document.createElement("div");
+          newRow.className = "row";
+
+          for (let i = 0; i < tam; i++) {
+              let newDiv = document.createElement("div");
+              newDiv.className = "caja";
+              newRow.appendChild(newDiv);
+          }
+          container.appendChild(newRow);
+      }
+  }
+
+  changeSize(tam); // Carga inicial
